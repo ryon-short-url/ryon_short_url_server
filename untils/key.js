8b0cdca57c988b9336ff2bfb5eee3c939ebf20a2
rsu <ryon.link@gmail.com>
2023-04-06 22:47:07 +0700

    git add file key

---
+++ untils/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleClientID: '496143531107-aqnn3sgpa30bd19dhlm1jie7q7fkq3lg.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    secret: "bezkoder-secret-key"
+};
